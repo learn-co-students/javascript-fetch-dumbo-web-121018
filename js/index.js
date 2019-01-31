bbf052396dc928849f2380849d8136f1ccae0136
kenneth-yu <kyu22@buffalo.edu>
2019-01-31 11:26:16 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+const token = '73f7c53bcb67f6a1b15ca384c047a668990fe2ba';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
