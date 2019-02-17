ffe0679d5df1d06a92c9099f2463ed6c694d81b6
lsamano <leizl.samano@gmail.com>
2019-02-16 21:38:49 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+const token = "b00a8ac4c8f1d9d3940aa92dd4d2ca7af8e1b37e"
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
